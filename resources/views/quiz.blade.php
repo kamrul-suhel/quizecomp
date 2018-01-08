dc2a9bd5e2285a74390db8795f6f107c9a9f156e
Ian Lainchbury <>
2018-01-08 17:47:27 +0000

    Sizing text

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -13,7 +13,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" type="text/css" href="css/jquery.fullpage.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/quiz.css?ver=1.01" />
+    <link rel="stylesheet" type="text/css" href="css/quiz.css?ver=1.2" />
 </head>
 <body>
 <div id="fullpage">
